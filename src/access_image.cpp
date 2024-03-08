4bbae264aa05f13e91177f1bbf0b168e45fdf7b7
Thomas A. Ciarfuglia <thomas.ciarfuglia@gmail.com>
2024-03-08 08:13:47 +0100

    ongoing preparations

--- src/access_image.cpp
+++ src/access_image.cpp
@@ -10,10 +10,10 @@
 // returns the 0-based location of the pixel value in the data array
 int pixel_address(const Image &im, int x, int y, int ch) {
 
-    int pixel_idx = 0;
-    int ch_size = im.w * im.h;
-    pixel_idx = (ch * ch_size) + (y * im.w) + x;
-    return pixel_idx;
+    // TODO: calculate and return the index
+
+    NOT_IMPLEMENTED();
+    return 0;
 }
 
 // HW0 #1
@@ -23,14 +23,8 @@ int pixel_address(const Image &im, int x, int y, int ch) {
 float get_clamped_pixel(const Image &im, int x, int y, int ch) {
     // TODO: clamp the coordinates and return the correct pixel value
 
-    if (x < 0) x = 0;
-    if (x >= im.w) x = im.w - 1;
-    if (y < 0) y = 0;
-    if (y >= im.h) y = im.h - 1;
-    if (ch < 0) ch = 0;
-    if (ch >= im.c) ch = im.c - 1;
-
-    return im.data[pixel_address(im, x, y, ch)];
+    NOT_IMPLEMENTED();
+    return 0.0;
 }
 
 
@@ -38,8 +32,9 @@ float get_clamped_pixel(const Image &im, int x, int y, int ch) {
 // Image& im: input image
 // int x,y,ch: pixel coordinates and channel of interest
 void set_pixel(Image &im, int x, int y, int c, float value) {
-    if ((x >= 0 && x < im.w) && (y >= 0 && y < im.h) && (c >= 0 && c < im.c))
-        im.data[pixel_address(im, x, y, c)] = value;
+    // TODO: Only set the pixel to the value if it's inside the image
+
+    NOT_IMPLEMENTED();
 }
 
 
@@ -51,24 +46,8 @@ void copy_image(Image &to, const Image &from) {
     // allocating data for the new image
     to.data = (float *) calloc(from.w * from.h * from.c, sizeof(float));
     to.c = from.c;
-    to.w = from.w;
-    to.h = from.h;
+    // TODO: populate the remaining fields in 'to' and copy the data
 
-
-
-    //TODO: check why memcpy is slower
-    
-    // test with for loop to show the difference in computational time
-    auto start_t = std::chrono::system_clock::now();
-    memcpy(to.data, from.data, to.size()*sizeof(float));
-    auto end_t1 = std::chrono::system_clock::now();
-    auto memcpy_time = end_t1 - start_t;
-    for (int i = 0; i < to.size(); ++i) {
-        to.data[i] = from.data[i];
-    }
-
-    auto end_t2 = std::chrono::system_clock::now();
-    auto forloop_time = end_t2 - end_t1;
-    cout << "memcpy ms: " << memcpy_time.count() << endl;
-    cout << "for loop ms: " << forloop_time.count() << endl;
+    // You might want to check how 'memcpy' function works
+    NOT_IMPLEMENTED();
 }
--- src/process_image.cpp
+++ src/process_image.cpp
@@ -12,15 +12,12 @@ using namespace std;
 // return the corresponding grayscale image
 Image rgb_to_grayscale(const Image &im) {
     assert(im.c == 3); // only accept RGB images
-    Image gray(im.w, im.h, 1); // create a new grayscale image (note: 1 channel)
+    Image gray(im.w,im.h,1); // create a new grayscale image (note: 1 channel)
 
-    // TODO: optimize
-    for (int i = 0; i < im.w; ++i) {
-        for (int j = 0; j < im.h; ++j) {
-            gray(i, j, 0) = 0.299 * im(i, j, 0) + 0.587 * im(i, j, 1) + 0.114 * im(i, j, 2);
-        }
+    // TODO: calculate the pixels of 'gray'
 
-    }
+
+    NOT_IMPLEMENTED();
 
     return gray;
 }
@@ -47,17 +44,11 @@ Image grayscale_to_rgb(const Image &im, float r, float g, float b) {
 // int c: which channel to shift
 // float v: how much to shift
 void shift_image(Image &im, int c, float v) {
-    assert(c >= 0 && c < im.c); // needs to be a valid channel
+    assert(c>=0 && c<im.c); // needs to be a valid channel
 
     // TODO: shift all the pixels at the specified channel
-    int ch_size = im.w*im.h;
-    for (int i = 0; i < im.w; ++i) {
-        for (int j = 0; j < im.h; ++j) {
-            im(i,j,c) += v;
-        }
-    }
 
-
+    NOT_IMPLEMENTED();
 }
 
 // HW0 #8
@@ -65,15 +56,11 @@ void shift_image(Image &im, int c, float v) {
 // int c: which channel to scale
 // float v: how much to scale
 void scale_image(Image &im, int c, float v) {
-    assert(c >= 0 && c < im.c); // needs to be a valid channel
+    assert(c>=0 && c<im.c); // needs to be a valid channel
 
     // TODO: scale all the pixels at the specified channel
-    int ch_size = im.w*im.h;
-    for (int i = 0; i < im.w; ++i) {
-        for (int j = 0; j < im.h; ++j) {
-            im(i,j,c) *= v;
-        }
-    }
+
+    NOT_IMPLEMENTED();
 
 }
 
@@ -82,20 +69,8 @@ void scale_image(Image &im, int c, float v) {
 // Image& im: input image to be modified in-place
 void clamp_image(Image &im) {
     // TODO: clamp all the pixels in all channel to be between 0 and 1
-    for (int k = 0; k < im.c; ++k) {
-        for (int i = 0; i < im.w; ++i) {
-            for (int j = 0; j < im.h; ++j) {
-                if (im(i,j,k) > 1) {
-                    im(i,j,k)=1;
-                }
-                if (im(i,j,k)<0){
-                    im(i,j,k)=0;
-                }
-            }
-        }
-    }
 
-
+    NOT_IMPLEMENTED();
 }
 
 // These might be handy
@@ -111,118 +86,46 @@ float min(float a, float b, float c) {
 // HW0 #6
 // Image& im: input image to be modified in-place
 void rgb_to_hsv(Image &im) {
-    assert(im.c == 3 && "only works for 3-channels images");
+    assert(im.c==3 && "only works for 3-channels images");
 
     // TODO: Convert all pixels from RGB format to HSV format
-    float V, S, H;
-    float m, C;
-    float RGB[3];
-    float &R = RGB[0];
-    float &G = RGB[1];
-    float &B = RGB[2];
-    int value_idx;
-    for (int i = 0; i < im.w; ++i) {
-        for (int j = 0; j < im.h; ++j) {
-            RGB[0] = im(i,j,0);
-            RGB[1] = im(i,j,1);
-            RGB[2] = im(i,j,2);
-            // Value
-            value_idx = max_element(RGB, RGB+3) - RGB;
-            V = RGB[value_idx];
-            // Saturation
-            m = *min_element(RGB, RGB+3);
-            C = V - m;
-            if (V==0){
-                S = 0;
-            }
-            else {
-                S = C / V;
-            }
-            // Hue
-            if (C == 0) {
-                H = 0;
-            }
-            else {
-                switch (value_idx) {
-                    case 0:
-                        H = (G-B)/C;
-                        break;
-                    case 1:
-                        H = (B-R)/C+2;
-                        break;
-                    case 2:
-                        H = (R-G)/C+4;
-                        break;
-                }
-            }
-            H = H/6;
-            if (H<0) H++;
-            // write the new color space values to im
-            im(i,j,0)=H;
-            im(i,j,1)=S;
-            im(i,j,2)=V;
-        }
-    }
+
+    NOT_IMPLEMENTED();
 
 }
 
 // HW0 #7
 // Image& im: input image to be modified in-place
 void hsv_to_rgb(Image &im) {
-    assert(im.c == 3 && "only works for 3-channels images");
+    assert(im.c==3 && "only works for 3-channels images");
 
     // TODO: Convert all pixels from HSV format to RGB format
-    float H, S, V;
-    float C, X, m;
-    float R, G, B;
-    for (int i = 0; i < im.w; ++i) {
-        for (int j = 0; j < im.h; ++j) {
-            H = im(i,j,0);
-            S = im(i,j,1);
-            V = im(i,j,2);
-            C = V * S;
-            X = C * (1-abs(fmod(6*H, 2) - 1));
-            m = V - C;
-            if (H<1.0/6) {
-                R = C;
-                G = X;
-                B = 0;
-            }
-            else if(H<2.0/6) {
-                R = X;
-                G = C;
-                B = 0;
-            }
-            else if (H<3.0/6) {
-                R = 0;
-                G = C;
-                B = X;
-            }
-            else if (H<4.0/6) {
-                R = 0;
-                G = X;
-                B = C;
-            }
-            else if (H<5.0/6) {
-                R = X;
-                G = 0;
-                B = C;
-            }
-            else {
-                R = C;
-                G = 0;
-                B = X;
-            }
-            R+=m;
-            G+=m;
-            B+=m;
-            // update image values
-            im(i,j,0) = R;
-            im(i,j,1) = G;
-            im(i,j,2) = B;
-        }
-    }
 
+    NOT_IMPLEMENTED();
+}
+
+// HW0 #9
+// Image& im: input image to be modified in-place
+void rgb_to_lch(Image& im)
+{
+    assert(im.c==3 && "only works for 3-channels images");
+
+    // TODO: Convert all pixels from RGB format to LCH format
+
+
+    NOT_IMPLEMENTED();
+
+}
+
+// HW0 #9
+// Image& im: input image to be modified in-place
+void lch_to_rgb(Image& im)
+{
+    assert(im.c==3 && "only works for 3-channels images");
+
+    // TODO: Convert all pixels from LCH format to RGB format
+
+    NOT_IMPLEMENTED();
 
 }
 
